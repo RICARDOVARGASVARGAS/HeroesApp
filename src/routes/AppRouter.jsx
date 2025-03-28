2bc6fcaa279c47f4fff0d6a2d7044801b7396c0d
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-03-28 14:48:01 -0500

    Learn

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,19 +5,16 @@ import { DcScreen } from "../components/dc/DcScreen";
 import { SearchScreen } from "../components/search/SearchScreen";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { Navbar } from "../components/ui/Navbar";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
-        
         <Routes>
-          <Route path="/" element={<MarvelScreen />} />
-          <Route path="/marvel" element={<MarvelScreen />} />
-          <Route path="/dc" element={<DcScreen />} />
-          <Route path="/search" element={<SearchScreen />} />
           <Route path="/login" element={<LoginScreen />} />
+
+          <Route path="/*" element={<DashboardRoutes />} />
         </Routes>
       </BrowserRouter>
     </div>
